cf89a1b2b4b025a2ece21784f59d70632b870e24
Koji Ino <kojiino@outlook.com>
2025-11-23 13:54:49 -1000

    finished pcb + firmware

---
+++ README.md
@@ -0,0 +1,3 @@
+# 17 Key Macropad
+
+This macropad is built to be as compact of an square as possible. It has 17 keys with rgb backlights.
---
+++ firmware/optimized_macropad/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2E, 0x0B, 0xE6, 0x25, 0x47, 0x7A, 0x74, 0x20}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define VIAL_TAP_DANCE_ENTRIES 6
+
+
+#define TAPPING_TERM 100
